3fa599f5628873b74090304eaac05528f8edcbc3
Kedrebeogo Deogratias <Kedrebeogodeogratias@aol.com>
2025-11-04 11:13:09 +0100

    add playlist

---
+++ Playlist.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Playlist {
+    private int idPlaylist;
+    private boolean publique;
+    private ArrayList<Musique> musiques;
+
+    public Playlist(int idPlaylist, boolean publique) {
+        this.idPlaylist = idPlaylist;
+        this.publique = publique;
+        this.musiques = new ArrayList<>();
+    }
+
+    public int getIdPlaylist() {
+        return idPlaylist;
+    }
+
+    public boolean isPublique() {
+        return publique;
+    }
+
+    public void setPublique(boolean publique) {
+        this.publique = publique;
+    }
+
+    public ArrayList<Musique> getMusiques() {
+        return musiques;
+    }
+
+    public void addMusique(Musique musique) {
+        this.musiques.add(musique);
+    }
+
+    // Méthode du diagramme
+    public void changementPubliquePrivee() {
+        this.publique = !this.publique;
+    }
+}
